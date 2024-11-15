6189732ee0a48b23d494b34c4f4580f10f6520ee
NdolwaVincent <131792083+NdolwaVincent@users.noreply.github.com>
2024-11-15 23:52:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-function goTopage(pagerUrl){
-	window.location.href= pagerUrl;
+
+function RegPage(){
+	window.location.href = "Register.html"
 }
+function RegPages(){
+	window.location.href = "Register.html"
+}
+let menuBtn = document.querySelector(".menu_btn");
+let navLinks = document.querySelector(".links");
+let menuBtnIcon = document.querySelector(".menu_btn i");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+
+  let isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "fas fa-times" : "fas fa-bars");
+});
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "fas fa-bars");
+});
